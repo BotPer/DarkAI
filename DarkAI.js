771d2291d3a1fedf27334c5f6be2e29c0bd60fe8
pluzels <140618835+pluzels@users.noreply.github.com>
2024-11-03 07:39:45 +0700

    Update DarkAI.js

--- DarkAI.js
+++ DarkAI.js
@@ -5,7 +5,7 @@ class DarkAI {
     constructor() {
         this.url = 'https://www.aiuncensored.info';
         this.apiEndpoint = 'https://darkai.foundation/chat';
-        this.defaultModel = 'gpt-4o';
+        this.defaultModel = 'ai_uncensored';
         this.models = [
             this.defaultModel, // Uncensored
             'gpt-3.5-turbo', // Uncensored
