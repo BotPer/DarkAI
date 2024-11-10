df7ea841bbce989057980309ab46ff9e392d2c66
YazidZet <168035657+YazidDizdd@users.noreply.github.com>
2024-11-10 13:21:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/api/chat', async (req, res) => {
     const { model, message } = req.query;
 
     if (!message) {
-        return res.status(400).json({ error: 'Message tidak boleh kosong.' });
+        return res.status(400).json({ error: 'Tcho ça fait pitié' });
     }
 
     try {
@@ -20,10 +20,10 @@ app.get('/api/chat', async (req, res) => {
         res.status(200).json({ response: result });
     } catch (error) {
         console.error('Terjadi kesalahan:', error);
-        res.status(500).json({ error: 'Terjadi kesalahan saat memproses permintaan.' });
+        res.status(500).json({ error: 'Pauvre tapette, ton code est mal foutue' });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`);
+    console.log(`Yeah Dude, ton serveur est Nickel ! ${PORT}`);
 });
